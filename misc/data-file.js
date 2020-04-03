fbe5c9fe75e014dc03c7e0d0f96ffd6fd4f65ff9
Mihir Chaturvedi <plibither8@gmail.com>
2020-04-03 17:07:58 +0530

    Fix: Directly use GitHub Gist API

--- misc/data-file.js
+++ misc/data-file.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const {writeFileSync, readFileSync} = require('fs');
 
-const Octokit = require('@octokit/rest');
 const fetch = require('node-fetch');
 
 // GitHub Gists env variables
@@ -10,24 +9,42 @@ const {
 	GITHUB_TOKEN
 } = process.env;
 
+// 'download' || 'upload'
+const CHOICE = process.argv[2];
+
+const GH_API_URL = 'https://api.github.com/gists/' + GIST_ID;
+
 // stupid config for keeping things simple
 const localFileNames = {
 	'lastfm-top-artists.json': 'data.json',
 	'unique-artists.json': 'artists.json'
 };
 
-// 'download' || 'upload'
-const CHOICE = process.argv[2];
+const getFetchOptions = (method, authString, body) => {
+	const base64AuthString = Buffer.from(authString).toString('base64');
+	return {
+		method,
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': 'Basic ' + base64AuthString
+		},
+		...(body ? {body: JSON.stringify(body)} : {})
+	}
+}
 
-const download = async octokit => {
-	const gist = await octokit.gists.get({ gist_id: GIST_ID });
-	for (const [fileName, file] of Object.entries(gist.data.files)) {
+const download = async () => {
+	const gist = await fetch(
+		GH_API_URL,
+		getFetchOptions('GET', GITHUB_TOKEN)
+	).then(res => res.json());
+
+	for (const [fileName, file] of Object.entries(gist.files)) {
 		const filePath = path.join(__dirname, '../', localFileNames[fileName]);
 		writeFileSync(filePath, file.content);
 	}
 }
 
-const upload = async octokit => {
+const upload = async () => {
 	const files = {};
 	for (const [remote, local] of Object.entries(localFileNames)) {
 		const filePath = path.join(__dirname, '../', local);
@@ -38,21 +55,19 @@ const upload = async octokit => {
 		};
 	}
 
-	await octokit.gists.update({
-		gist_id: GIST_ID,
-		files
-	});
+	await fetch(
+		GH_API_URL,
+		getFetchOptions('PATCH', GITHUB_TOKEN, {files})
+	);
 }
 
 (async () => {
-	const octokit = new Octokit({ auth: `token ${GITHUB_TOKEN}` }) // Instantiate Octokit
-
 	switch (CHOICE) {
 		case 'download':
-			await download(octokit);
+			await download();
 			break;
 		case 'upload':
-			await upload(octokit);
+			await upload();
 			break;
 	}
 })();
